3b8305362e202ccddf2c84d7453dc349ed87fa90
Holly <hnguyen0711@gmail.com>
2017-01-17 17:47:31 -0800

    added jquery plugin masonry

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,4 +17,11 @@
 //= require bootstrap
 //= require_tree .
 
+// jQuery
+var $container = jQuery('#masonry-grid');
+// initialize
+$container.masonry({
+  columnWidth: 200,
+  itemSelector: '.grid-item'
+});
 
