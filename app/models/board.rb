f9391098d67c5c540f96aa0a20c4d036a765ee82
Holly <hnguyen0711@gmail.com>
2017-01-18 15:03:40 -0800

    can edit board images

--- app/models/board.rb
+++ app/models/board.rb
@@ -5,4 +5,14 @@ class Board < ActiveRecord::Base
 
 	has_attached_file :image, styles: { medium: "500x500>", thumb: "250x250>" }, presence: true
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+	before_validation { image.clear if @delete_image }
+
+	 def delete_image
+	   @delete_image ||= false
+	 end
+
+	 def delete_image=(value)
+	   @delete_image  = !value.to_i.zero?
+	 end
 end
